ffa35e215513db3ca0e12b550b919b9af6e06a0a
Jill Williams <codeGeek.03.14@gmail.com>
2018-10-28 12:15:30 -0600

    Add files via upload

---
+++ ducks_animation_project/ducks.js
@@ -0,0 +1,10 @@
+function showImage (src, width, height, alt);
+var blackhelmet = document.createElement("player");
+    img.src = .win_the_day_player.png,
+    img.width = auto;
+    img.height = auto;
+    img.alt = alt;
+    document.body.appendChild (img.png);
+}
+    <!-- /* // This next line will just add it to the <body> tag -->
+    <!-- // but you can adapt to make it append to the element you want. */ -->
